8e1cfe5f148d89aeecdf26d5e0bf345a6794163a
Michinded <michinded@gmail.com>
2024-06-11 15:59:47 -0600

    Se agregó la plantilla de React y Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Usuarios\UserContoller;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get(
-    // Url de la ruta
-    '/usuarios/todos',
-    // Controlador y método
-    [UserContoller::class, 'all']
-);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/hola', function () {
-    return 'Hola Mundo';
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get(
-    '/hola/{nombre}',
-    function ($nombre) {
-        return 'Hola ' . $nombre;
-    }
-);
+require __DIR__.'/auth.php';
